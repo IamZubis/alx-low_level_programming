f4b2ea389f20f0e231f71779c99e1b792dd90447
Zubis <devikovo@gmail.com>
2022-03-11 19:58:13 +0100

    rask 5&6 ans

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return:1
@@ -9,4 +10,3 @@ char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, ch, sizeof(ch));
 return (1);
 }
-
